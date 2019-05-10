f89e03894186adf54a524dadcd69513e09172565
kirsnik <gwingster@gmail.com>
2019-05-09 19:58:06 -0700

    modified for project

--- src/components/Shelf/ProductList/Product/index.js
+++ src/components/Shelf/ProductList/Product/index.js
@@ -33,13 +33,9 @@ const Product = ({ product, addProduct }) => {
       onClick={() => addProduct(product)}
       data-sku={product.sku}
     >
-      {product.isFreeShipping && (
-        <div className="shelf-stopper">Free shipping</div>
-      )}
       <Thumb
         classes="shelf-item__thumb"
         src={require(`../../../../static/products/${product.sku}_1.jpg`)}
-        alt={product.title}
       />
       <p className="shelf-item__title">{product.title}</p>
       <div className="shelf-item__price">
